bedf20e0fc5d9f2b04617db9c3905564b66551dd
AthiwatJantasri <athiwat242515@gmail.com>
2026-01-31 15:03:06 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
-
 import (
 	"fmt"
 	"math"
 )
 
+//interface Shaper
 type Shaper interface {
   Area() float64
   Perimeter() float64
   Info()
 }
 
+// class Rectangle
 type Rectangle struct {
   width float64
   height float64
@@ -21,37 +22,41 @@ func(r Rectangle) Area() float64 {
 }
 
 func(r Rectangle) Perimeter() float64 {
-  return 2 * (r.width + r.height)
+  return 2.0 * (r.width + r.height)
 }
 
-func(r Rectangle) Info(){
+func (r Rectangle) Info() {
   fmt.Printf("Area=%.2f, Perimeter=%.2f\n", r.Area(), r.Perimeter())
 }
+
+
+//class Circle
 
 type Circle struct {
   radius float64
 }
 
-func (c Circle) Area() float64 {
-  return math.Pi * c.radius
+func(c Circle) Area() float64 {
+  return math.Pi * c.radius * c.radius
 }
 
-func (c Circle) Perimeter() float64 {
+func(c Circle) Perimeter() float64 {
   return 2 * math.Pi * c.radius
 }
 
-func(c Circle) Info(){
+func (c Circle) Info() {
   fmt.Printf("Area=%.2f, Perimeter=%.2f\n", c.Area(), c.Perimeter())
 }
 
 
 
 func main() {
-	r := Rectangle{width: 4, height: 5}
+  fmt.Printf("__RectangleInfo__")
+	r := Rectangle{ width: 5, height: 5 }
 	r.Info()
-
-	fmt.Println("------------")
-
-	c := Circle{radius: 3}
+	fmt.Println("============================")
+	
+	fmt.Println("__CircleInfo__")
+	c := Circle{ radius: 10 }
 	c.Info()
 }
